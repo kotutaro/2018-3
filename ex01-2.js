7f3df87d0ce2401c8337e0a155590612b5f96539
zm17498 <zm17498@josai.ac.jp>
2019-01-27 20:49:31 -0500

    message

---
+++ ex01-2.js
@@ -0,0 +1,14 @@
+function nazo(){
+  if(document.getElementById('answer').value == 'ミニチュアシュナウザー'){
+    alert('あたり!');
+  }
+  else{alert('はずれ');}
+}
+
+
+function nazo2(){
+  if(document.getElementById('answer').value == '女の子'){
+    alert('あたり!');
+  }
+  else{alert('はずれ');}
+}
---
+++ ex01-4.js
@@ -0,0 +1,11 @@
+function txtchange1(){
+  document.getElementById('txt1').innerHTML='ソフトボール';
+}
+
+function txtchange2(){
+  document.getElementById('txt2').innerHTML='どんな種類も';
+}
+
+function txtchange3(){
+  document.getElementById('txt3').innerHTML='お肉料理全般';
+}
